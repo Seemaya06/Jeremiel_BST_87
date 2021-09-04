b495e94a8b292112bae51dd93b87f95a5f954a21
Seemaya06 <71091580+Seemaya06@users.noreply.github.com>
2021-09-04 05:36:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYMPTTapM_DdPfIOhXtKg9syyZ2esfkB4",
+  authDomain: "book-story-telling-app.firebaseapp.com",
+  databaseURL: "https://book-story-telling-app-default-rtdb.firebaseio.com",
+  projectId: "book-story-telling-app",
+  storageBucket: "book-story-telling-app.appspot.com",
+  messagingSenderId: "196180929596",
+  appId: "1:196180929596:web:6b560113f210003fc2e739"
+};
